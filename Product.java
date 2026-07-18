c62f59588b227adca7a3022d3bd7e1fbfadb06ea
darrenmax9 <148580346+darrenmax9@users.noreply.github.com>
2026-07-18 15:40:14 -0400

    Update Product.java

---
+++ Product.java
@@ -0,0 +1,31 @@
+public class Product {
+    private String code;
+    private String description;
+    private double price;
+
+    public Product() {
+    }
+
+    public Product(String code, String description, double price) {
+        this.code = code;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return code + "\t" + description + "\t" + price;
+    }
+}
